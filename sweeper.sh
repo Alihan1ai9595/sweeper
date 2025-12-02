265ba82983e240a8e322d6ec455cadf791e3e164
Alihan1ai9595 <112238568+Alihan1ai9595@users.noreply.github.com>
2025-12-02 22:57:50 +0500

    Rename script from Cleaner.sh to Sweeper

--- sweeper.sh
+++ sweeper.sh
@@ -5,7 +5,7 @@
 # MM    MM  aaa aa  dddddd  eeeee    bbbbbb       yy     aaa aa lll 111 hh   hh 333333  nn   nn
 #                                             yyyyy
 # Support - al1h3n(tg,ds) | Donate me - paypal.me/al1h3n
-# Cleaner.sh v1.0.1 - FIxed flatpak prompts.
+# Sweeper v1.0.1 - FIxed flatpak prompts.
 # Part of the Cleanus Pack.
 # ==============================================================================
 
